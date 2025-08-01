8bc050c9bf4af428fc7fda04989a54b7f8cc859c
prcysprk <prcysprk@umich.edu>
2025-08-01 19:53:35 -0400

    Add notes

--- rank.py
+++ rank.py
@@ -33,7 +33,7 @@ def calc_relevance(scores, k):
     return relevance
 '''
 
-def rank_value(artifacts, lvls, persist, targets, CHANGE=True, k=1, num_trials=1000, rng=None, seed=None):
+def rank_value(artifacts, lvls, persist, targets, CHANGE=True, k=2, num_trials=1000, rng=None, seed=None):
     num_artifacts = len(artifacts)
     try:
         _ = iter(lvls)
@@ -135,7 +135,7 @@ def rank_value(artifacts, lvls, persist, targets, CHANGE=True, k=1, num_trials=1
         
     return relevance
 
-def rank_entropy(artifacts, lvls, persist, targets, CHANGE=True, k=1, num_trials=1000, rng=None, seed=None):
+def rank_entropy(artifacts, lvls, persist, targets, CHANGE=True, k=1, num_trials=200, rng=None, seed=None):
     num_artifacts = len(artifacts)
     try:
         _ = iter(lvls)
@@ -224,7 +224,6 @@ def rank_entropy(artifacts, lvls, persist, targets, CHANGE=True, k=1, num_trials
             
             distro_maxed = persist['distros_maxed'][changed]
             if len(distro_maxed) != len(upgrades):
-                print('reshape')
                 persist['distros_maxed'][changed] = np.zeros((len(upgrades), num_trials, 19), dtype=np.uint8)
 
             for i, upgrade in enumerate(upgrades):
@@ -291,46 +290,244 @@ def rank_entropy(artifacts, lvls, persist, targets, CHANGE=True, k=1, num_trials
         temp = scores[i].copy()
         
         scores[i] = 0
+        
+        '''
         if k == 1:
-            base_cutoff = scores.max(axis=0)
+            k_cutoff = scores.max(axis=0)
         else:
-            base_cutoff = np.partition(scores, -k, axis=0)[-k]
+            k_cutoff, k_1_cutoff = np.partition(scores, [-k, -k+1], axis=0)[-k]
+            
+        k_above = scores > k_cutoff[None, ...]
+        k_above_count = np.count_nonzero(k_above, axis=0)
+        
+        k_eq    = scores == k_cutoff[None, ...]
+        k_tie_counts = np.count_nonzero(k_eq, axis=0)
+        
+        k_1_above = scores > k_1_cutoff[None, ...]
+        k_1_above_count = np.count_nonzero(k_1_above, axis=0)
+        
+        k_1_eq = scores == k_1_cutoff[None, ...]
+        k_1_tie_counts = np.count_nonzero(k_1_eq, axis=0)    
+        '''
+        
+            
 
-        above = scores > base_cutoff[None, ...]
-        above_count = np.count_nonzero(above, axis=0)
-        #above_count = above.sum(axis=0)
-        leftover = k - above_count
+        '''
+        k_above = scores > k_cutoff[None, ...]
+        k_above_count = np.count_nonzero(k_above, axis=0)
+        k_leftover = k - k_above_count
 
-        eq    = scores == base_cutoff[None, ...]
-
-        #tie_counts = eq.sum(axis=0)
-        tie_counts = np.count_nonzero(eq, axis=0)
-        frac_per_tie = np.where(tie_counts > 0, leftover / tie_counts, 0)
-        eq_contrib = eq * frac_per_tie[None, ...]
-        #base_relevance = above.sum(axis=1).astype(float)
-        base_relevance = np.count_nonzero(above, axis=1).astype(float)
+        k_eq    = scores == k_cutoff[None, ...]
+        k_tie_counts = np.count_nonzero(k_eq, axis=0)
+        k_frac_per_tie = np.where(k_tie_counts > 0, k_leftover / k_tie_counts, 0)
+        eq_contrib = k_eq * k_frac_per_tie[None, ...]
+        base_relevance = np.count_nonzero(k_above, axis=1).astype(float)
         base_relevance += eq_contrib.sum(axis=1)
         
         base_ent = entropy(base_relevance, axis=0)
+        '''
         
         
         for upgrade, prob in zip(distros_scores[i], probs):
             if prob == 0:
                 continue
             
+            '''
+            precompute:
+            mask for where current is = (k - 1) threshold
+            mask for where current is = k treshold
+            
+            mask for where new is > (k - 1) threshold
+            mask for where new is = (k - 1) threshold
+            mask for where new is = k threshold
+            
+            
+            precompute entropy from other artifacts for each possible case
+            '''
+            
+            
+            
+            '''
+            
+            INSERTING
+            
+            if in top k:
+                if in top k - 1:
+                    top k, change
+                if in edge k - 1:
+                    if new is in k - 1:
+                        edge of k, cutoff is old (k - 1)
+                    if new is in edge k - 1:
+                        edge of k, cutoff is old (k - 1), edge size increments
+                    if new is not in k - 1:
+                        top k, no change
+            if in edge k
+                if top k - 1 == top k:
+                    if new is in top k:
+                        edge of k, cutoff is old (k - 1), no change
+                    if new is in edge:
+                        edge of k, cutoff is old (k - 1), edge size increments
+                if top k - 1 != top k:
+                    if new is in top k:
+                        not in k
+                    if new is in edge k:
+                        edge of k, edge size increments
+            if not in k:
+                not in k
+                    
+                
+            if in top k:
+                if in top k - 1:
+                    A
+                if in edge k - 1:
+                    if new is in k - 1:
+                        B
+                    if new is in edge k - 1:
+                        C
+                    if new is not in k - 1:
+                        A
+            if in edge k
+                if top k - 1 == top k:
+                    if new is in top k:
+                        B
+                    if new is in edge:
+                        C
+                if top k - 1 != top k:
+                    if new is in top k:
+                        D
+                    if new is in edge k:
+                        E
+            if not in k:
+                D
+                    
+                    
+            A: 
+                if in top k - 1
+                if in edge k - 1 AND new is not in k - 1
+            B:
+                if in edge k - 1 AND new is in k - 1
+            C:
+                if in edge k - 1 AND new is in edge k - 1
+            D: 
+                if in edge k AND not in edge k - 1 AND new is in top k
+                if not in k
+            E:
+                if in edge k AND not in edge k - 1 AND new is in edge k
+            
+            
+            
+                
+            in top k - 1:
+                top k
+            in edge k - 1:
+                new is in k - 1
+                    edge of k, cutoff is old (k - 1) 
+                new is in edge k - 1
+                    edge of k, cutoff is old (k - 1), edge size increments
+                new is not in k - 1
+                    top k
+            in edge k:
+                new is in k
+                    not in k
+                new is in edge k
+                    edge of k, edge size increments
+            not in k:
+                not in k
+                
+                
+            new is in k - 1:
+                in edge k - 1:
+                    edge of k, cutoff is old (k - 1)
+                in edge k:
+                    not in k
+            new is in edge k - 1:
+                in edge k - 1:
+                    edge of k, cutoff is old (k - 1), edge size increments
+                in edge k:
+                    not in k
+            new is in edge k:
+                in edge k:
+                    edge of k, edge size increments
+                
+            
+            
+            
+            
+                
+            top k: 
+                if in top k - 1
+                if in edge k - 1 AND new is not in k - 1
+            edge of k, cutoff is old (k - 1):
+                if in edge k - 1 AND new is in k - 1
+            edge of k, cutoff is old (k - 1), edge size increments:
+                if in edge k - 1 AND new is in edge k - 1
+            not in k:
+                if in edge k AND not in edge k - 1 AND new is in top k
+                if not in k
+            edge of k, edge size increments:
+                if in edge k AND not in edge k - 1 AND new is in edge k
+            '''
+            
+            '''
+            
+            DELETING
+            
+            old is in k:
+                if k cutoff == k + 1 cutoff:
+                    nothing
+                else:    
+                    in edge k:
+                        in k
+                    in edge k + 1:
+                        in edge k, cutoff is old (k + 1)
+            old is in edge k:
+                if k cutoff == k + 1 cutoff:
+                    decrement edge
+                else:
+                    in edge k:
+                        in k
+                    in edge k + 1:
+                        in edge k, cutoff is old (k + 1)
+            
+            in top k:
+                top k
+            in edge k:
+                if in edge of k + 1:
+                    nothing happens
+                is not in k + 1:
+                    in k
+            in edge k + 1:
+                edge of k, cutoff is old (k + 1)
+            not in k + 1:
+                not in k
+            
+            
+            
+            if in top k:
+                top k
+            if in edge k:
+                if top k == top k + 1:
+                    
+                if top k != top k + 1:
+            if not in k:
+                if not in top k + 1:
+                    not in k
+                if in edge k + 1:
+                    if old was in k:
+                        edge of k, cutoff is old (k + 1)
+                    if old was in edge k:
+                        edge of k, cutoff is old (k + 1)
+            '''
+            
             temp_check = False
             
             '''
-            greater = upgrade > base_cutoff
-            eq = upgrade == base_cutoff
-            less = upgrade < base_cutoff
+            greater = upgrade > k_cutoff
+            eq = upgrade == k_cutoff
+            less = upgrade < k_cutoff
             
             useless = np.count_nonzero(less, axis=0) == num_trials
             '''
-            
-            if not np.any(upgrade >= base_cutoff):
-                information_gain[i] -= prob * base_ent
-                continue
             
             scores[i] = upgrade # (num_trials, num_targets)
             
@@ -348,7 +545,7 @@ def rank_entropy(artifacts, lvls, persist, targets, CHANGE=True, k=1, num_trials
 
             #tie_counts = eq.sum(axis=0)
             tie_counts = np.count_nonzero(eq, axis=0)
-            frac_per_tie = np.where(tie_counts > 0, leftover / tie_counts, 0)
+            frac_per_tie = leftover / tie_counts
             eq_contrib = eq * frac_per_tie[None, ...]
             #relevance = above.sum(axis=1).astype(float)
             relevance = np.count_nonzero(above, axis=1).astype(float)
@@ -374,14 +571,19 @@ def rank_entropy(artifacts, lvls, persist, targets, CHANGE=True, k=1, num_trials
     if np.max(information_gain) == 0:
         print('qpweoifhjap')
         persist = {}
-        return rank_entropy(artifacts, lvls, persist, targets, CHANGE, k, num_trials * 2, rng)
+        asdf =  rank_entropy(artifacts, lvls, persist, targets, CHANGE, k, num_trials * 2, rng)
+        persist = {}
+        return asdf
     
     relevance = np.sum(information_gain, axis=1)
+    
+    #relevance /= np.where(lvls == 20, 1, UPGRADE_REQ_EXP[lvls])
     
     if np.sum(information_gain) == 0:
         print('qpweoifhjap')
     
     if CHANGE:
+        relevance[lvls == 20] = 0
         changed = np.argmax(relevance)
         persist['changed'] = changed
         print()
@@ -396,7 +598,7 @@ if __name__ == '__main__':
     '''
     '''
     start = time.time()
-    num = 1
+    num = 10
     totals = np.zeros(num)
     time_avg = np.zeros(num)
     #targets = {'atk_': 6, 'atk': 2, 'crit_': 8}
@@ -418,8 +620,8 @@ if __name__ == '__main__':
     )
 
     for i in range(num):
-        artifacts = generate('flower', size=20, seed=i)
-        totals[i] = (simulate_exp(artifacts, np.zeros(20, dtype=int), targets, rank_entropy))
+        artifacts = generate('flower', size=200, seed=i)
+        totals[i] = (simulate_exp(artifacts, np.zeros(200, dtype=int), targets, rank_entropy))
         print(i, totals[i])
         print()
 
@@ -433,7 +635,7 @@ if __name__ == '__main__':
     
     '''
     start = time.time()
-    filename = 'artifacts/genshinData_GOOD_2025_07_26_19_37.json'
+    filename = 'artifacts/genshinData_GOOD_2025_07_31_18_01.json'
     artifacts, slots, rarities, lvls, sets = load(filename)
     relevant = rate(artifacts, slots, rarities, lvls, sets, rank_value, num=100)
     
